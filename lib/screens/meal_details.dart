42c4c65fcde5c0df76fcb9920714026428ade5dc
Chetan Rathod <chetan0101career@gmail.com>
2024-02-24 18:05:22 +0530

    meal deatils and favourite done

---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatefulWidget {
+  const MealDetailsScreen(
+      {super.key,
+      required this.meal,
+      required this.onToggleFavourite,
+      required this.favouriteMeals});
+
+  final Meal meal;
+  final void Function(Meal meal) onToggleFavourite;
+  final List<Meal> favouriteMeals;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MealDetailsScreenState();
+  }
+}
+
+class _MealDetailsScreenState extends State<MealDetailsScreen> {
+  Widget _FavouriteIcon(meal) {
+    final isExisting = widget.favouriteMeals.contains(meal);
+    if (isExisting) {
+      return const Icon(Icons.star_outlined,color: Color.fromARGB(255, 255, 239, 95),);
+    } else {
+      return const Icon(Icons.star_border);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              widget.onToggleFavourite(widget.meal);
+            },
+            icon: _FavouriteIcon(widget.meal),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              widget.meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Ingredients",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final ingredient in widget.meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final step in widget.meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favouriteMeals = [];
+
+  void _onToggleFavourite(Meal meal) {
+    final isExisting = _favouriteMeals.contains(meal);
+
+    if (isExisting) {
+      setState(() {
+        _favouriteMeals.remove(meal);
+      });
+    } else {
+      setState(() {
+        _favouriteMeals.add(meal);
+      });
+    }
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String activePageTitle = 'Categories';
+    Widget activePage = CategoriesScreen(onToggleFavourite: _onToggleFavourite, favouriteMeals: _favouriteMeals,);
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: _favouriteMeals, onToggleFavourite: _onToggleFavourite, favouriteMeals: _favouriteMeals,);
+      activePageTitle = 'Your Favourites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Your Favourites',
+          ),
+        ],
+        currentIndex: _selectedPageIndex,
+      ),
+    );
+  }
+}
